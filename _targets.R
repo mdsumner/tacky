0b16e402f84fd2c6866ba94b086e142d63873dc7
Michael Sumner <mdsumner@gmail.com>
2024-04-10 11:30:14 +0000

    small tweaks

--- _targets.R
+++ _targets.R
@@ -34,17 +34,19 @@ list(
   tar_target(ex, c(-64, -63.5, -9, -8.5)),
   tar_target(srcs, hrefs(stacit(ex, date = "2020-01"))),
 
-  tar_target(spec, list(dimension = c(1280, 0), ex = ex, crs = "EPSG:4326")),
+  tar_target(spec0, list(dimension = c(1280, 0), ex = ex, crs = "EPSG:4326")),
   #
   #   ## we need a template, these might be different so we normalize upfront
-  tar_target(ref, warpfun(srcs$aot[1], ext = spec$ex, dim = spec$dimension, crs = spec$crs)),
+  tar_target(ref, warpfun(srcs$aot[1], ext = spec0$ex, dim = spec0$dimension, crs = spec0$crs)),
+  tar_target(spec, list(ex = attr(ref, "gis")$bbox[c(1, 3, 2, 4)],
+                        dimension = attr(ref, "gis")$dim[1:2], crs = attr(ref, "gis")$srs)),
   tar_target(red, srcs$red),
   tar_target(green, srcs$green),
   tar_target(blue, srcs$blue),
   tar_target(cloud, srcs$cloud),
 
-  tar_target(d, sclfun(red, green, blue, cloud, ext = attr(ref, "gis")$bbox[c(1, 3, 2, 4)],
-                       crs = attr(ref, "gis")$srs, dim = attr(ref, "gis")$dim[1:2]),
+  tar_target(d, sclfun(red, green, blue, cloud, ext = spec$ex,
+                       crs = spec$crs, dim = spec$dimension[1:2]),
              pattern = map(red, green, blue, cloud),
              iteration = "list")
 
